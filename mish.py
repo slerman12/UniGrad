d273e2004e243d9e38580c6955e0f5a8117319f6
sam <slerman12@gmail.com>
2020-07-27 15:57:56 -0400

    cuda

--- mish.py
+++ mish.py
@@ -354,8 +354,8 @@ learning_rate = 0.001
 device = torch.device('cuda:0' if torch.cuda.is_available() else "cpu")
 print(torch.cuda.is_available())
 
-# for activation_choice in ["R_LeakyReLU_ReLU", "R_Mish_ReLU", "LeakyReLU", "mish", "swish", "relu"]:
-for activation_choice in ["R_Mish_ReLU", "LeakyReLU", "mish", "swish", "relu"]:
+for activation_choice in ["R_LeakyReLU_ReLU", "R_Mish_ReLU", "LeakyReLU", "mish", "swish", "relu"]:
+# for activation_choice in ["R_Mish_ReLU", "LeakyReLU", "mish", "swish", "relu"]:
 
     model = densenet121(activation = activation_choice)
 
