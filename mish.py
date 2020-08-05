e687fedbe2f1d84c43efa7cf4b822e4af05069c3
sam <slerman12@gmail.com>
2020-08-05 17:21:38 -0400

    runs

--- mish.py
+++ mish.py
@@ -698,101 +698,105 @@ learning_rate = 0.001
 device = torch.device('cuda:0' if torch.cuda.is_available() else "cpu")
 print(torch.cuda.is_available())
 
-for activation_choice in ["R_LeakyReLU_ReLU", "R_Mish_ReLU", "LeakyReLU", "mish", "swish", "relu"]:
-# for activation_choice in ["R_Mish_ReLU", "LeakyReLU", "mish", "swish", "relu"]:
-    if args.model == "densenet121":
-        model = densenet121(activation = activation_choice).to(device)
-    elif args.model == "densenet169":
-        model = densenet169(activation = activation_choice).to(device)
-    elif args.model == "densenet201":
-        model = densenet201(activation = activation_choice).to(device)
-    elif args.model == "densenet161":
-        model = densenet161(activation = activation_choice).to(device)
-    elif args.model == "inceptionv3":
-        model = inceptionv3(activation = activation_choice)
+for name in ["1", "2", "3"]:
+    args.name = name
+    for model in ["inceptionv3", "densenet201", "densenet169", "densenet161"]:
+        args.model = model
+        for activation_choice in ["R_LeakyReLU_ReLU", "R_Mish_ReLU", "LeakyReLU", "mish", "swish", "relu"]:
+        # for activation_choice in ["R_Mish_ReLU", "LeakyReLU", "mish", "swish", "relu"]:
+            if args.model == "densenet121":
+                model = densenet121(activation = activation_choice).to(device)
+            elif args.model == "densenet169":
+                model = densenet169(activation = activation_choice).to(device)
+            elif args.model == "densenet201":
+                model = densenet201(activation = activation_choice).to(device)
+            elif args.model == "densenet161":
+                model = densenet161(activation = activation_choice).to(device)
+            elif args.model == "inceptionv3":
+                model = inceptionv3(activation = activation_choice)
 
-    # set loss function
-    criterion = nn.CrossEntropyLoss()
+            # set loss function
+            criterion = nn.CrossEntropyLoss()
 
-    # set optimizer, only train the classifier parameters, feature parameters are frozen
-    optimizer = Adam(model.parameters(), lr=learning_rate)
+            # set optimizer, only train the classifier parameters, feature parameters are frozen
+            optimizer = Adam(model.parameters(), lr=learning_rate)
 
-    train_stats = pd.DataFrame(columns = ['Epoch', 'Time per epoch', 'Avg time per step', 'Train loss', 'Train accuracy', 'Train top-3 accuracy','Test loss', 'Test accuracy', 'Test top-3 accuracy'])
+            train_stats = pd.DataFrame(columns = ['Epoch', 'Time per epoch', 'Avg time per step', 'Train loss', 'Train accuracy', 'Train top-3 accuracy','Test loss', 'Test accuracy', 'Test top-3 accuracy'])
 
-    #train the model
-    model.to(device)
+            #train the model
+            model.to(device)
 
-    steps = 0
-    running_loss = 0
-    for epoch in range(epochs):
+            steps = 0
+            running_loss = 0
+            for epoch in range(epochs):
 
-        since = time.time()
+                since = time.time()
 
-        train_accuracy = 0
-        top3_train_accuracy = 0
-        for inputs, labels in trainloader:
-            steps += 1
-            # Move input and label tensors to the default device
-            inputs, labels = inputs.to(device), labels.to(device)
+                train_accuracy = 0
+                top3_train_accuracy = 0
+                for inputs, labels in trainloader:
+                    steps += 1
+                    # Move input and label tensors to the default device
+                    inputs, labels = inputs.to(device), labels.to(device)
 
-            optimizer.zero_grad()
+                    optimizer.zero_grad()
 
-            logps = model.forward(inputs).to(device)
-            loss = criterion(logps, labels)
-            loss.backward()
-            optimizer.step()
+                    logps = model.forward(inputs).to(device)
+                    loss = criterion(logps, labels)
+                    loss.backward()
+                    optimizer.step()
 
-            running_loss += loss.item()
+                    running_loss += loss.item()
 
-            # calculate train top-1 accuracy
-            ps = torch.exp(logps)
-            top_p, top_class = ps.topk(1, dim=1)
-            equals = top_class == labels.view(*top_class.shape)
-            train_accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+                    # calculate train top-1 accuracy
+                    ps = torch.exp(logps)
+                    top_p, top_class = ps.topk(1, dim=1)
+                    equals = top_class == labels.view(*top_class.shape)
+                    train_accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
 
-            # Calculate train top-3 accuracy
-            np_top3_class = ps.topk(3, dim=1)[1].cpu().numpy()
-            target_numpy = labels.cpu().numpy()
-            top3_train_accuracy += np.mean([1 if target_numpy[i] in np_top3_class[i] else 0 for i in range(0, len(target_numpy))])
+                    # Calculate train top-3 accuracy
+                    np_top3_class = ps.topk(3, dim=1)[1].cpu().numpy()
+                    target_numpy = labels.cpu().numpy()
+                    top3_train_accuracy += np.mean([1 if target_numpy[i] in np_top3_class[i] else 0 for i in range(0, len(target_numpy))])
 
-        time_elapsed = time.time() - since
+                time_elapsed = time.time() - since
 
-        test_loss = 0
-        test_accuracy = 0
-        top3_test_accuracy = 0
-        model.eval()
-        with torch.no_grad():
-            for inputs, labels in testloader:
-                inputs, labels = inputs.to(device), labels.to(device)
-                logps = model.forward(inputs)
-                batch_loss = criterion(logps, labels)
+                test_loss = 0
+                test_accuracy = 0
+                top3_test_accuracy = 0
+                model.eval()
+                with torch.no_grad():
+                    for inputs, labels in testloader:
+                        inputs, labels = inputs.to(device), labels.to(device)
+                        logps = model.forward(inputs)
+                        batch_loss = criterion(logps, labels)
 
-                test_loss += batch_loss.item()
+                        test_loss += batch_loss.item()
 
-                # Calculate test top-1 accuracy
-                ps = torch.exp(logps)
-                top_p, top_class = ps.topk(1, dim=1)
-                equals = top_class == labels.view(*top_class.shape)
-                test_accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+                        # Calculate test top-1 accuracy
+                        ps = torch.exp(logps)
+                        top_p, top_class = ps.topk(1, dim=1)
+                        equals = top_class == labels.view(*top_class.shape)
+                        test_accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
 
-                # Calculate test top-3 accuracy
-                np_top3_class = ps.topk(3, dim=1)[1].cpu().numpy()
-                target_numpy = labels.cpu().numpy()
-                top3_test_accuracy += np.mean([1 if target_numpy[i] in np_top3_class[i] else 0 for i in range(0, len(target_numpy))])
+                        # Calculate test top-3 accuracy
+                        np_top3_class = ps.topk(3, dim=1)[1].cpu().numpy()
+                        target_numpy = labels.cpu().numpy()
+                        top3_test_accuracy += np.mean([1 if target_numpy[i] in np_top3_class[i] else 0 for i in range(0, len(target_numpy))])
 
-        print(f"Epoch {epoch+1}/{epochs}.. "
-              f"Time per epoch: {time_elapsed:.4f}.. "
-              f"Average time per step: {time_elapsed/len(trainloader):.4f}.. "
-              f"Train loss: {running_loss/len(trainloader):.4f}.. "
-              f"Train accuracy: {train_accuracy/len(trainloader):.4f}.. "
-              f"Top-3 train accuracy: {top3_train_accuracy/len(trainloader):.4f}.. "
-              f"Test loss: {test_loss/len(testloader):.4f}.. "
-              f"Test accuracy: {test_accuracy/len(testloader):.4f}.. "
-              f"Top-3 test accuracy: {top3_test_accuracy/len(testloader):.4f}")
+                print(f"Epoch {epoch+1}/{epochs}.. "
+                      f"Time per epoch: {time_elapsed:.4f}.. "
+                      f"Average time per step: {time_elapsed/len(trainloader):.4f}.. "
+                      f"Train loss: {running_loss/len(trainloader):.4f}.. "
+                      f"Train accuracy: {train_accuracy/len(trainloader):.4f}.. "
+                      f"Top-3 train accuracy: {top3_train_accuracy/len(trainloader):.4f}.. "
+                      f"Test loss: {test_loss/len(testloader):.4f}.. "
+                      f"Test accuracy: {test_accuracy/len(testloader):.4f}.. "
+                      f"Top-3 test accuracy: {top3_test_accuracy/len(testloader):.4f}")
 
-        train_stats = train_stats.append({'Epoch': epoch, 'Time per epoch':time_elapsed, 'Avg time per step': time_elapsed/len(trainloader), 'Train loss' : running_loss/len(trainloader), 'Train accuracy': train_accuracy/len(trainloader), 'Train top-3 accuracy':top3_train_accuracy/len(trainloader),'Test loss' : test_loss/len(testloader), 'Test accuracy': test_accuracy/len(testloader), 'Test top-3 accuracy':top3_test_accuracy/len(testloader)}, ignore_index=True)
+                train_stats = train_stats.append({'Epoch': epoch, 'Time per epoch':time_elapsed, 'Avg time per step': time_elapsed/len(trainloader), 'Train loss' : running_loss/len(trainloader), 'Train accuracy': train_accuracy/len(trainloader), 'Train top-3 accuracy':top3_train_accuracy/len(trainloader),'Test loss' : test_loss/len(testloader), 'Test accuracy': test_accuracy/len(testloader), 'Test top-3 accuracy':top3_test_accuracy/len(testloader)}, ignore_index=True)
 
-        running_loss = 0
-        model.train()
+                running_loss = 0
+                model.train()
 
-    train_stats.to_csv('train_log_{}_{}_{}.csv'.format(model, activation_choice, args.name))
+            train_stats.to_csv('train_log_{}_{}_{}.csv'.format(model, activation_choice, args.name))
