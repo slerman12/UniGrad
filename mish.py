307cb6e2d475d21125952948fbdb5008e27e09de
sam <slerman12@gmail.com>
2020-08-19 20:31:13 -0400

    small batch size

--- mish.py
+++ mish.py
@@ -189,7 +189,7 @@ class R_LeakyReLU_ReLU(torch.autograd.Function):
     @staticmethod
     def forward(ctx, input):
         ctx.save_for_backward(input)
-        relu_inputs.extend(input.tolist())
+        relu_inputs.extend(np.mean(input.tolist()))
         return input.clamp(min=0)
 
     @staticmethod
