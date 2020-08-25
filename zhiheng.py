1cc473f88a288d848a03d1997f8a61cc2cd5cb50
sam <slerman12@gmail.com>
2020-08-25 10:27:13 -0400

    zhiheng

--- zhiheng.py
+++ zhiheng.py
@@ -1,18 +1,10 @@
 import argparse
-
-import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-import os
-
 import time
-
-# import pytorch
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.optim import SGD,Adam,lr_scheduler
-from torch.utils.data import random_split
 import torchvision
 from torchvision import transforms, datasets
 from torch.utils.data import DataLoader
@@ -180,11 +172,6 @@ class R_Mish_ReLU(torch.autograd.Function):
         grad_input[(input < 0) * (0 <= grad_input)] = 0
         return grad_input
 
-relu_inputs = []
-prev_val_score = 0
-val_increases = []
-relu_input_avgs = []
-
 the_coef = None
 
 class R_LeakyReLU_ReLU(torch.autograd.Function):
@@ -291,7 +278,7 @@ class Transition(nn.Module):
         self.down_sample = nn.Sequential(
             nn.BatchNorm2d(in_channels),
             # UniGrad(autograd_func=R_Mish_ReLU),
-            # nn.ReLU(inplace=True),
+            nn.ReLU(inplace=True),
             nn.Conv2d(in_channels, out_channels, 1, bias=False),
             nn.AvgPool2d(2, stride=2)
         )
@@ -388,364 +375,24 @@ def densenet201(activation = 'relu'):
 def densenet161(activation = 'relu'):
     return DenseNet(Bottleneck, [6,12,36,24], growth_rate=48, activation = activation)
 
-class BasicConv2d(nn.Module):
-
-    def __init__(self, input_channels, output_channels, activation = 'relu', **kwargs):
-        super().__init__()
-        self.conv = nn.Conv2d(input_channels, output_channels, bias=False, **kwargs)
-        self.bn = nn.BatchNorm2d(output_channels)
-
-        if activation == 'relu':
-            self.relu = nn.ReLU(inplace=True)
-
-        if activation == 'LeakyReLU':
-            self.relu = nn.LeakyReLU(inplace=True)
-
-        if activation == 'swish':
-            self.relu = swish()
-
-        if activation == 'mish':
-            self.relu = mish()
-
-        if activation == "R_LeakyReLU_ReLU":
-            self.relu = UniGrad(autograd_func=R_LeakyReLU_ReLU)
-
-        if activation == "R_Mish_ReLU":
-            self.relu = UniGrad(autograd_func=R_Mish_ReLU)
-
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.bn(x)
-        x = self.relu(x)
-
-        return x
-
-#same naive inception module
-class InceptionA(nn.Module):
-
-    def __init__(self, input_channels, pool_features, activation = 'relu'):
-        super().__init__()
-        self.branch1x1 = BasicConv2d(input_channels, 64, kernel_size=1, activation = activation)
-
-        self.branch5x5 = nn.Sequential(
-            BasicConv2d(input_channels, 48, kernel_size=1, activation = activation),
-            BasicConv2d(48, 64, kernel_size=5, padding=2, activation = activation)
-        )
-
-        self.branch3x3 = nn.Sequential(
-            BasicConv2d(input_channels, 64, kernel_size=1, activation = activation),
-            BasicConv2d(64, 96, kernel_size=3, padding=1, activation = activation),
-            BasicConv2d(96, 96, kernel_size=3, padding=1, activation = activation)
-        )
-
-        self.branchpool = nn.Sequential(
-            nn.AvgPool2d(kernel_size=3, stride=1, padding=1),
-            BasicConv2d(input_channels, pool_features, kernel_size=3, padding=1, activation = activation)
-        )
-
-    def forward(self, x):
-
-        #x -> 1x1(same)
-        branch1x1 = self.branch1x1(x)
-
-        #x -> 1x1 -> 5x5(same)
-        branch5x5 = self.branch5x5(x)
-        #branch5x5 = self.branch5x5_2(branch5x5)
-
-        #x -> 1x1 -> 3x3 -> 3x3(same)
-        branch3x3 = self.branch3x3(x)
-
-        #x -> pool -> 1x1(same)
-        branchpool = self.branchpool(x)
-
-        outputs = [branch1x1, branch5x5, branch3x3, branchpool]
-
-        return torch.cat(outputs, 1)
-
-#downsample
-#Factorization into smaller convolutions
-class InceptionB(nn.Module):
-
-    def __init__(self, input_channels, activation = 'relu'):
-        super().__init__()
-
-        self.branch3x3 = BasicConv2d(input_channels, 384, kernel_size=3, stride=2, activation = activation)
-
-        self.branch3x3stack = nn.Sequential(
-            BasicConv2d(input_channels, 64, kernel_size=1, activation = activation),
-            BasicConv2d(64, 96, kernel_size=3, padding=1, activation = activation),
-            BasicConv2d(96, 96, kernel_size=3, stride=2, activation = activation)
-        )
-
-        self.branchpool = nn.MaxPool2d(kernel_size=3, stride=2)
-
-    def forward(self, x):
-
-        #x - > 3x3(downsample)
-        branch3x3 = self.branch3x3(x)
-
-        #x -> 3x3 -> 3x3(downsample)
-        branch3x3stack = self.branch3x3stack(x)
-
-        #x -> avgpool(downsample)
-        branchpool = self.branchpool(x)
-
-        #"""We can use two parallel stride 2 blocks: P and C. P is a pooling
-        #layer (either average or maximum pooling) the activation, both of
-        #them are stride 2 the filter banks of which are concatenated as in
-        #figure 10."""
-        outputs = [branch3x3, branch3x3stack, branchpool]
-
-        return torch.cat(outputs, 1)
-
-#Factorizing Convolutions with Large Filter Size
-class InceptionC(nn.Module):
-    def __init__(self, input_channels, channels_7x7, activation = 'relu'):
-        super().__init__()
-        self.branch1x1 = BasicConv2d(input_channels, 192, kernel_size=1, activation = activation)
-
-        c7 = channels_7x7
-
-        #In theory, we could go even further and argue that one can replace any n × n
-        #convolution by a 1 × n convolution followed by a n × 1 convolution and the
-        #computational cost saving increases dramatically as n grows (see figure 6).
-        self.branch7x7 = nn.Sequential(
-            BasicConv2d(input_channels, c7, kernel_size=1, activation = activation),
-            BasicConv2d(c7, c7, kernel_size=(7, 1), padding=(3, 0), activation = activation),
-            BasicConv2d(c7, 192, kernel_size=(1, 7), padding=(0, 3), activation = activation)
-        )
-
-        self.branch7x7stack = nn.Sequential(
-            BasicConv2d(input_channels, c7, kernel_size=1, activation = activation),
-            BasicConv2d(c7, c7, kernel_size=(7, 1), padding=(3, 0), activation = activation),
-            BasicConv2d(c7, c7, kernel_size=(1, 7), padding=(0, 3), activation = activation),
-            BasicConv2d(c7, c7, kernel_size=(7, 1), padding=(3, 0), activation = activation),
-            BasicConv2d(c7, 192, kernel_size=(1, 7), padding=(0, 3), activation = activation)
-        )
-
-        self.branch_pool = nn.Sequential(
-            nn.AvgPool2d(kernel_size=3, stride=1, padding=1),
-            BasicConv2d(input_channels, 192, kernel_size=1, activation = activation),
-        )
-
-    def forward(self, x):
-
-        #x -> 1x1(same)
-        branch1x1 = self.branch1x1(x)
-
-        #x -> 1layer 1*7 and 7*1 (same)
-        branch7x7 = self.branch7x7(x)
-
-        #x-> 2layer 1*7 and 7*1(same)
-        branch7x7stack = self.branch7x7stack(x)
-
-        #x-> avgpool (same)
-        branchpool = self.branch_pool(x)
-
-        outputs = [branch1x1, branch7x7, branch7x7stack, branchpool]
-
-        return torch.cat(outputs, 1)
-
-class InceptionD(nn.Module):
-
-    def __init__(self, input_channels, activation = 'relu'):
-        super().__init__()
-
-        self.branch3x3 = nn.Sequential(
-            BasicConv2d(input_channels, 192, kernel_size=1, activation = activation),
-            BasicConv2d(192, 320, kernel_size=3, stride=2, activation = activation)
-        )
-
-        self.branch7x7 = nn.Sequential(
-            BasicConv2d(input_channels, 192, kernel_size=1, activation = activation),
-            BasicConv2d(192, 192, kernel_size=(1, 7), padding=(0, 3), activation = activation),
-            BasicConv2d(192, 192, kernel_size=(7, 1), padding=(3, 0), activation = activation),
-            BasicConv2d(192, 192, kernel_size=3, stride=2, activation = activation)
-        )
-
-        self.branchpool = nn.AvgPool2d(kernel_size=3, stride=2)
-
-    def forward(self, x):
-
-        #x -> 1x1 -> 3x3(downsample)
-        branch3x3 = self.branch3x3(x)
-
-        #x -> 1x1 -> 1x7 -> 7x1 -> 3x3 (downsample)
-        branch7x7 = self.branch7x7(x)
-
-        #x -> avgpool (downsample)
-        branchpool = self.branchpool(x)
-
-        outputs = [branch3x3, branch7x7, branchpool]
-
-        return torch.cat(outputs, 1)
-
-
-#same
-class InceptionE(nn.Module):
-    def __init__(self, input_channels, activation = 'relu'):
-        super().__init__()
-        self.branch1x1 = BasicConv2d(input_channels, 320, kernel_size=1, activation = activation)
-
-        self.branch3x3_1 = BasicConv2d(input_channels, 384, kernel_size=1, activation = activation)
-        self.branch3x3_2a = BasicConv2d(384, 384, kernel_size=(1, 3), padding=(0, 1), activation = activation)
-        self.branch3x3_2b = BasicConv2d(384, 384, kernel_size=(3, 1), padding=(1, 0), activation = activation)
-
-        self.branch3x3stack_1 = BasicConv2d(input_channels, 448, kernel_size=1, activation = activation)
-        self.branch3x3stack_2 = BasicConv2d(448, 384, kernel_size=3, padding=1, activation = activation)
-        self.branch3x3stack_3a = BasicConv2d(384, 384, kernel_size=(1, 3), padding=(0, 1), activation = activation)
-        self.branch3x3stack_3b = BasicConv2d(384, 384, kernel_size=(3, 1), padding=(1, 0), activation = activation)
-
-        self.branch_pool = nn.Sequential(
-            nn.AvgPool2d(kernel_size=3, stride=1, padding=1),
-            BasicConv2d(input_channels, 192, kernel_size=1, activation = activation)
-        )
-
-    def forward(self, x):
-
-        #x -> 1x1 (same)
-        branch1x1 = self.branch1x1(x)
-
-        # x -> 1x1 -> 3x1
-        # x -> 1x1 -> 1x3
-        # concatenate(3x1, 1x3)
-        #"""7. Inception modules with expanded the filter bank outputs.
-        #This architecture is used on the coarsest (8 × 8) grids to promote
-        #high dimensional representations, as suggested by principle
-        #2 of Section 2."""
-        branch3x3 = self.branch3x3_1(x)
-        branch3x3 = [
-            self.branch3x3_2a(branch3x3),
-            self.branch3x3_2b(branch3x3)
-        ]
-        branch3x3 = torch.cat(branch3x3, 1)
-
-        # x -> 1x1 -> 3x3 -> 1x3
-        # x -> 1x1 -> 3x3 -> 3x1
-        #concatenate(1x3, 3x1)
-        branch3x3stack = self.branch3x3stack_1(x)
-        branch3x3stack = self.branch3x3stack_2(branch3x3stack)
-        branch3x3stack = [
-            self.branch3x3stack_3a(branch3x3stack),
-            self.branch3x3stack_3b(branch3x3stack)
-        ]
-        branch3x3stack = torch.cat(branch3x3stack, 1)
-
-        branchpool = self.branch_pool(x)
-
-        outputs = [branch1x1, branch3x3, branch3x3stack, branchpool]
-
-        return torch.cat(outputs, 1)
-
-class InceptionV3(nn.Module):
-
-    def __init__(self, num_classes=10, activation = 'relu'):
-        super().__init__()
-        self.Conv2d_1a_3x3 = BasicConv2d(3, 32, kernel_size=3, padding=1, activation = activation)
-        self.Conv2d_2a_3x3 = BasicConv2d(32, 32, kernel_size=3, padding=1, activation = activation)
-        self.Conv2d_2b_3x3 = BasicConv2d(32, 64, kernel_size=3, padding=1, activation = activation)
-        self.Conv2d_3b_1x1 = BasicConv2d(64, 80, kernel_size=1, activation = activation)
-        self.Conv2d_4a_3x3 = BasicConv2d(80, 192, kernel_size=3, activation = activation)
-
-        #naive inception module
-        self.Mixed_5b = InceptionA(192, pool_features=32, activation = activation)
-        self.Mixed_5c = InceptionA(256, pool_features=64, activation = activation)
-        self.Mixed_5d = InceptionA(288, pool_features=64, activation = activation)
-
-        #downsample
-        self.Mixed_6a = InceptionB(288, activation = activation)
-
-        self.Mixed_6b = InceptionC(768, channels_7x7=128, activation = activation)
-        self.Mixed_6c = InceptionC(768, channels_7x7=160, activation = activation)
-        self.Mixed_6d = InceptionC(768, channels_7x7=160, activation = activation)
-        self.Mixed_6e = InceptionC(768, channels_7x7=192, activation = activation)
-
-        #downsample
-        self.Mixed_7a = InceptionD(768, activation = activation)
-
-        self.Mixed_7b = InceptionE(1280, activation = activation)
-        self.Mixed_7c = InceptionE(2048, activation = activation)
-
-        #6*6 feature size
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.dropout = nn.Dropout2d()
-        self.linear = nn.Linear(2048, num_classes)
-
-    def forward(self, x):
-
-        #32 -> 30
-        x = self.Conv2d_1a_3x3(x)
-        x = self.Conv2d_2a_3x3(x)
-        x = self.Conv2d_2b_3x3(x)
-        x = self.Conv2d_3b_1x1(x)
-        x = self.Conv2d_4a_3x3(x)
-
-        #30 -> 30
-        x = self.Mixed_5b(x)
-        x = self.Mixed_5c(x)
-        x = self.Mixed_5d(x)
-
-        #30 -> 14
-        #Efficient Grid Size Reduction to avoid representation
-        #bottleneck
-        x = self.Mixed_6a(x)
-
-        #14 -> 14
-        #"""In practice, we have found that employing this factorization does not
-        #work well on early layers, but it gives very good results on medium
-        #grid-sizes (On m × m feature maps, where m ranges between 12 and 20).
-        #On that level, very good results can be achieved by using 1 × 7 convolutions
-        #followed by 7 × 1 convolutions."""
-        x = self.Mixed_6b(x)
-        x = self.Mixed_6c(x)
-        x = self.Mixed_6d(x)
-        x = self.Mixed_6e(x)
-
-        #14 -> 6
-        #Efficient Grid Size Reduction
-        x = self.Mixed_7a(x)
-
-        #6 -> 6
-        #We are using this solution only on the coarsest grid,
-        #since that is the place where producing high dimensional
-        #sparse representation is the most critical as the ratio of
-        #local processing (by 1 × 1 convolutions) is increased compared
-        #to the spatial aggregation."""
-        x = self.Mixed_7b(x)
-        x = self.Mixed_7c(x)
-
-        #6 -> 1
-        x = self.avgpool(x)
-        x = self.dropout(x)
-        x = x.view(x.size(0), -1)
-        x = self.linear(x)
-        return x
-
-
-def inceptionv3(activation = 'relu'):
-    return InceptionV3(activation = activation)
-
 trainloader = get_training_dataloader(train_transform)
 testloader = get_testing_dataloader(test_transform)
 
 epochs = 100
-# epochs = 200
 batch_size = 128
 learning_rate = 0.001
 device = torch.device('cuda:0' if torch.cuda.is_available() else "cpu")
 print(torch.cuda.is_available())
 
-for name in ["1", "2", "3"]:
-# for name in ["1", "2"]:
+for name in ["1",
+             # "2", "3"
+             ]:
     args.name = name
-    for model_name in ["densenet121", "densenet161", "densenet201", "densenet169"]:
-    # for model_name in ["densenet201", "densenet161"]:
+    for model_name in ["densenet121"
+        # , "densenet161", "densenet201", "densenet169"
+                       ]:
         args.model = model_name
-        # for activation_choice in ["R_LeakyReLU_ReLU", "R_Mish_ReLU", "LeakyReLU", "mish", "swish", "relu"]:
-        # for activation_choice in [0.1, 0.001, 0.3, 0.8, 0.005]:
-        # for activation_choice in ["R_LeakyReLU_ReLU", "relu"]:
-        for activation_choice in ["R_LeakyReLU_ReLU", "relu", 0.001, "LeakyReLU", "mish", "swish"]:
-        # for activation_choice in ["R_Mish_ReLU", "LeakyReLU", "mish", "swish", "relu"]:
+        for activation_choice in ["R_LeakyReLU_ReLU"]:
             if args.model == "densenet121":
                 model = densenet121(activation = activation_choice).to(device)
             elif args.model == "densenet169":
@@ -754,8 +401,6 @@ for name in ["1", "2", "3"]:
                 model = densenet201(activation = activation_choice).to(device)
             elif args.model == "densenet161":
                 model = densenet161(activation = activation_choice).to(device)
-            elif args.model == "inceptionv3":
-                model = inceptionv3(activation = activation_choice)
 
             # set loss function
             criterion = nn.CrossEntropyLoss()
@@ -838,15 +483,8 @@ for name in ["1", "2", "3"]:
                 print(args.model, args.name, activation_choice)
 
                 train_stats = train_stats.append({'Epoch': epoch, 'Time per epoch':time_elapsed, 'Avg time per step': time_elapsed/len(trainloader), 'Train loss' : running_loss/len(trainloader), 'Train accuracy': train_accuracy/len(trainloader), 'Train top-3 accuracy':top3_train_accuracy/len(trainloader),'Test loss' : test_loss/len(testloader), 'Test accuracy': test_accuracy/len(testloader), 'Test top-3 accuracy':top3_test_accuracy/len(testloader)}, ignore_index=True)
-                # val_increases = val_increases.append(test_accuracy/len(testloader) - prev_val_score)
-                # relu_input_avgs = relu_input_avgs.append(np.mean(relu_inputs))
-                # relu_inputs = []
-                # prev_val_score = test_accuracy/len(testloader)
 
                 running_loss = 0
                 model.train()
 
             train_stats.to_csv('train_log_{}_{}_{}.csv'.format(args.model, activation_choice, args.name))
-            # plt.plot(relu_input_avgs, val_increases, 'o', color='black')
-            # plt.savefig('inputs_vs_scores_plot_{}_{}_{}.png'.format(args.model, activation_choice, args.name))
-            # plt.close()
